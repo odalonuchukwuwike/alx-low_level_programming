9f87f202af292ee34684fd9d7e6233407acc4ade
Wike <chukzodalonu@gmail.com>
2023-10-09 04:26:44 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/*Using the puts function to print strings to our output console*/
+
+#inclde < stdio.h >
+main()
+{
+	puts ("Programming is like building a multilingual puzzle");
+
+	return (0);
+
+
+}
