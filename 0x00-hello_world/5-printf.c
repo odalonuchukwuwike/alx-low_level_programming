bec3b578fa7859a4eb3b0bb4d29bf6d62798e90c
Wike <chukzodalonu@gmail.com>
2023-10-10 03:54:37 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - A program that uses printf function to output a string
+ * Return: 0 (Using the printf function to print strings to our output console)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
