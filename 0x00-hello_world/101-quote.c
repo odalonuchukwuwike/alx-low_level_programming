92f5261246f6e2271bf4b923c92ca3f64aeb6627
Wike <chukzodalonu@gmail.com>
2023-10-11 02:47:40 -0700

    fput

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * Writing a program using putschar to output text
  * Return: 1
  */
-int main (void)
+int main(void)
 {
-fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
